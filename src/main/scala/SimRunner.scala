70ad0d4d575aa1bad5fe396968b76ae29f7dfcd7
Jordan Curzon <curzonj@gmail.com>
2015-07-13 01:18:15 -0700

    fetches json from redis list

--- src/main/scala/SimRunner.scala
+++ src/main/scala/SimRunner.scala
@@ -1,7 +1,14 @@
 import java.util.TimerTask
 import java.util.concurrent.{TimeUnit, ScheduledFuture, ScheduledThreadPoolExecutor}
-
+import org.json4s.jackson.JsonMethods
+import scredis.exceptions.RedisTransactionAbortedException
+import scala.concurrent.{ExecutionContext, Future}
+import scala.util.{Try, Success, Failure}
+import com.github.curzonj.RedisFactory
 import org.slf4j.LoggerFactory
+import ExecutionContext.Implicits.global
+import org.json4s._
+import org.json4s.jackson.JsonMethods._
 
 object SimRunner {
   def main(args: Array[String]): Unit = {
@@ -11,7 +18,6 @@ object SimRunner {
     println("scheduled")
   }
 
-  val pool = new ScheduledThreadPoolExecutor(1)
   val logger = LoggerFactory.getLogger(getClass)
   val task = new TimerTask {
     def run() =  worldTick
@@ -20,17 +26,32 @@ object SimRunner {
   val interval = 80
   var currentTick: Long = 0
 
+  lazy val pool = new ScheduledThreadPoolExecutor(1)
+  lazy val redis = RedisFactory.get
 
   def worldTick: Unit = {
     currentTick += interval
     val startedAt= System.currentTimeMillis
-    var jitter = startedAt - currentTick
+    val jitter = startedAt - currentTick
 
     printM("world tick", "tick" -> currentTick, "jitter" -> jitter, "ts" -> startedAt)
 
-    val endedAt = System.currentTimeMillis
-    val delay = currentTick + interval - endedAt
-    pool.schedule(task, delay, TimeUnit.MILLISECONDS)
+    val name = "command_at_"+currentTick
+    redis.withTransaction { t =>
+      t.rename("commands", name)
+      t.lRange[String](name)
+    } map { value =>
+      def optionalJson(s: String): Any = Try(parse(s)) getOrElse s
+      val obj = value map { v => optionalJson(v) }
+
+      println(obj)
+    } onComplete { _ =>
+      val endedAt = System.currentTimeMillis
+      printM("end tick", "duration" -> (endedAt - startedAt))
+
+      val delay = currentTick + interval - endedAt
+      pool.schedule(task, delay, TimeUnit.MILLISECONDS)
+    }
   }
 
   def printM(str: String, list: (String, Any)*): Unit = {
---
+++ src/main/scala/com/github/curzonj/RedisFactory.scala
@@ -0,0 +1,29 @@
+package com.github.curzonj
+
+import com.typesafe.config.{ConfigFactory, ConfigValueFactory}
+import org.slf4j.LoggerFactory
+import scredis._
+import scala.util.{ Success, Failure }
+import java.net.URI
+
+object RedisFactory {
+
+  val logger = LoggerFactory.getLogger(getClass)
+  val typesafeConfig = ConfigFactory.load()
+  val mergedConfig = sys.env.get("REDIS_URL").map { value =>
+    val uri = new URI(value)
+    logger.info("REDIS_URL={}", value)
+
+    typesafeConfig.withValue("scredis.redis.host",
+      ConfigValueFactory.fromAnyRef(uri.getHost))
+  }.getOrElse(typesafeConfig)
+
+  val redis = Redis(mergedConfig.getConfig("scredis"))
+
+  // Import internal ActorSystem's dispatcher (execution context) to register callbacks
+  import redis.dispatcher
+
+  def get: Redis = redis
+  def destroy = redis.quit()
+
+}
