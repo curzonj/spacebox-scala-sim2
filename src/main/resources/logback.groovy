13fdec5d9d47582b55aec759e7b60cca54962fdf
Jordan Curzon <curzonj@gmail.com>
2015-07-14 00:33:13 -0700

    simple dev logging format

--- src/main/resources/logback.groovy
+++ src/main/resources/logback.groovy
@@ -1,8 +1,14 @@
-appender("STDOUT", ConsoleAppender) {
+appender("STDOUT-1", ConsoleAppender) {
     encoder(PatternLayoutEncoder) {
         pattern = "%d{HH:mm:ss.SSS} [%thread] %-5level %logger{5} - %msg%n"
     }
 }
 
+appender("STDOUT-2", ConsoleAppender) {
+    encoder(PatternLayoutEncoder) {
+        pattern = "%msg%n"
+    }
+}
 
-root(DEBUG, [ "STDOUT" ])
+
+root(DEBUG, [ "STDOUT-2" ])
