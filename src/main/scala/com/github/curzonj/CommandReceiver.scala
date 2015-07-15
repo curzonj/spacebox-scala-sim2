31c3b5ae3b7b8c8b51a0d36bec255e973ead1408
Jordan Curzon <curzonj@gmail.com>
2015-07-15 04:25:07 -0700

    extract api patch into case class. play with kryo

--- src/main/scala/com/github/curzonj/CommandReceiver.scala
+++ src/main/scala/com/github/curzonj/CommandReceiver.scala
@@ -6,7 +6,6 @@ import scala.concurrent.{ExecutionContext, Future}
 import ExecutionContext.Implicits.global
 import scala.util.{Try, Success, Failure}
 import org.json4s._
-
 import org.json4s.jackson.JsonMethods._
 
 object CommandReceiver extends Logging {
@@ -14,16 +13,15 @@ object CommandReceiver extends Logging {
   lazy val redis = RedisFactory.get
   implicit val formats = DefaultFormats ++ JavaTypesSerializers.all
 
-  type ApiCommand = String
-  var lastCommandFetch: Future[List[ApiCommand]] = Future { List() }
-  def fetchCommands(thisTick: Long): Future[List[ApiCommand]] = {
+  var lastCommandFetch: Future[List[String]] = Future { List() }
+  def fetchCommands(thisTick: Long): Future[List[String]] = {
     val name = "command_at_"+thisTick
     val commandFetch = lastCommandFetch
 
     logger.trace("at=commandFetch:start ts={}", thisTick)
     lastCommandFetch = redis.withTransaction { t =>
       t.rename("commands", name)
-      t.lRange[ApiCommand](name)
+      t.lRange[String](name)
     }
 
     lastCommandFetch onComplete { _ =>
@@ -33,21 +31,42 @@ object CommandReceiver extends Logging {
     commandFetch
   }
 
-  case class DiffCommand(uuid: UUID) {
+  case class ApiVector3(x: Float, y: Float, z: Float)
+  case class ApiVector4(x: Float, y: Float, z: Float, w: Float)
+  case class ApiCommand(uuid: UUID, patch: ApiPatch) {
     var sourceJson: Option[JValue] = None
   }
+  case class ApiPatch(
+                     uuid: Option[UUID],
+                     `type`: String,
+                     solar_system: Option[UUID],
+                     agent_id: Option[UUID],
+                     blueprint: Option[UUID],
+                     position: Option[ApiVector3],
+                     velocity: Option[ApiVector3],
+                     chunk: Option[ApiVector3],
+                     facing: Option[ApiVector4],
+                     health: Int,
+                     model_name: String,
+                     systems: Map[String, JValue]
+                     )
 
-  def commands(tick: Long): Future[List[DiffCommand]] = {
+  def commands(tick: Long): Future[List[ApiCommand]] = {
     CommandReceiver.fetchCommands(tick) map { list =>
       list map { value =>
         val parsed = Try(parse(value))
         val extracted = parsed flatMap {
-          v => Try(v.extract[DiffCommand])
-        } map { diff =>
+          v => Try(v.extract[ApiCommand])
+        } map { cmd =>
+          val bytes = KryoFactory.serialize(cmd)
+          logger.debug("data sizes kryoDeflate={} json={}", bytes.size, value.getBytes.size)
+          var dser = KryoFactory.readObject(bytes, classOf[ApiCommand])
+          println(dser)
+
           // parsed.get can't throw an exception here because
           // Try.map only looks at successes
-          diff.sourceJson = Some(parsed.get)
-          diff
+          cmd.sourceJson = Some(parsed.get)
+          cmd
         }
 
         if (extracted.isFailure) logger.error("Failed to parse `"+value.toString+"`", extracted.failed.get)
---
+++ src/main/scala/com/github/curzonj/KryoFactory.scala
@@ -0,0 +1,39 @@
+package com.github.curzonj
+
+import java.util.zip.{InflaterInputStream, DeflaterOutputStream}
+import java.io.{ByteArrayInputStream, ByteArrayOutputStream}
+import com.twitter.chill.ScalaKryoInstantiator
+import com.esotericsoftware.kryo.Kryo
+import com.esotericsoftware.kryo.io.{Input, Output}
+
+/*
+  TODO: pool instead of TLS, we probably only ever need one
+  TODO: reuse the baos and the input/outputs also
+ */
+object KryoFactory {
+  def serialize(value: Any): Array[Byte] = {
+    val kryo = kryos.get
+    val baos = new ByteArrayOutputStream
+    val stream = new DeflaterOutputStream(baos)
+    val output = new Output(stream, 4096)
+    kryo.writeObject(output, value)
+    output.close()
+
+    baos.toByteArray
+  }
+
+  def readObject[T](data: Array[Byte], klass: Class[T]): T = {
+    val kryo = kryos.get
+    val stream = new InflaterInputStream(new ByteArrayInputStream(data))
+    val input = new Input(stream)
+    kryo.readObject(input, klass)
+  }
+
+  private val kryos: ThreadLocal[Kryo] = new ThreadLocal[Kryo] {
+    override def initialValue: Kryo =  {
+      val instantiator = new ScalaKryoInstantiator
+      instantiator.setRegistrationRequired(false)
+      instantiator.newKryo()
+    }
+  }
+}
